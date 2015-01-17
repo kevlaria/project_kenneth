4e00fba15870b601ddb16cf044199112828ff03d
Pedro Samora <psamora@outlook.com>
2015-01-17 16:11:16 -0500

    Edit page is different, still need to add update button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -15,10 +15,12 @@ class EventsController < ApplicationController
   # GET /events/new
   def new
     @event = Event.new
+    @where = "new"
   end
 
   # GET /events/1/edit
   def edit
+    @where = "edit"
   end
 
   # POST /events
