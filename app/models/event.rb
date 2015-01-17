3438a13eeb619d4e4558e15de3959304d5919eb2
Pedro Samora <psamora@outlook.com>
2015-01-17 01:01:10 -0500

    Started scheduler

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,24 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
+
+  def processOrders
+  	@now = Event.where(:time => 1.minute.ago..Time.now);
+  	@next = Event.where(:time => Time.now..1.hour.from_now);
+
+  	@now.each do |event|
+  		if event.type == "Order" then
+  			order = Order.find(event.event_id)
+  			order.notify
+  		end
+  	end
+
+  	@next.each do |event|
+   		if event.type == "Order" then
+   			order = Order.find(event.event_id)
+   			order.message
+  		end 	
+  	end
+  end
+
 end
