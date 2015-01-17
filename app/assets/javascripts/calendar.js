0aa7dd6fa7f465a49457bbfbcdf542d44c51df52
kevlaria <kevlaria@gmail.com>
2015-01-17 15:09:05 -0500

    Database events now appearing on calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,72 +1,10 @@
 function populate_calendar(){
-	data = get_events_from_rails();
-	console.log("data");
-	console.log(data);
-	
-		$('#calendar').fullCalendar({
-			defaultDate: '2014-11-12',
+	$('#calendar').fullCalendar({
+			defaultDate: new Date(),
 			editable: true,
 			eventLimit: true, // allow "more" link when too many events
-			events: [
-				{
-					title: 'All Day Event',
-					start: '2014-11-01'
-				},
-				{
-					title: 'Long Event',
-					start: '2014-11-07',
-					end: '2014-11-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2014-11-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2014-11-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2014-11-11',
-					end: '2014-11-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2014-11-12T10:30:00',
-					end: '2014-11-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2014-11-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2014-11-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2014-11-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2014-11-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2014-11-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2014-11-28'
-				}
-			]
-		});
-}
 
-function get_events_from_rails(){
-	data =  $.getJSON('/calendar/events.json');
-	return data;
+			events: '/calendar/events.json'
+	});	
+	
 }
