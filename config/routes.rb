0ca50450741a9b3ee0bbcaf9fb84d5033da5a065
Pedro Samora <psamora@outlook.com>
2015-01-17 18:43:43 -0500

    Bunch of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :weathers
+
   resources :orders
 
   resources :events
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150117174501) do
+ActiveRecord::Schema.define(version: 20150117231919) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -69,5 +69,10 @@ ActiveRecord::Schema.define(version: 20150117174501) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "weathers", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
